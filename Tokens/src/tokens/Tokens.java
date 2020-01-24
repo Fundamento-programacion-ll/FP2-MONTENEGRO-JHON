d70f9c42a45bd05b3627c95ff7225a58c98a1bd2
JhonAndres <JhonAndres@users.noreply.github.com>
2020-01-24 09:46:38 -0500

    Tokens

--- Tokens/src/tokens/Tokens.java
+++ Tokens/src/tokens/Tokens.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tokens;
 
 import java.util.Arrays;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Bryan
- */
 public class Tokens {
 
     /**
