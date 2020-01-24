718eba4274e61c0fa45de223f9aa42c1cc767154
JhonAndres <JhonAndres@users.noreply.github.com>
2020-01-24 09:04:28 -0500

    Tokens

---
+++ Tokens/src/tokens/Vector.java
@@ -0,0 +1,46 @@
+package tokens;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
+
+public class Vector {
+    
+    private int vector[];
+
+    public int[] getVector() {
+        return vector;
+    }
+
+    public void setVector() {
+        
+        String datosVector; 
+        datosVector = JOptionPane.showInputDialog(null, "Ingrese los Datos separados por coma");
+        StringTokenizer tokkens=new StringTokenizer(datosVector, ","); //datos,separador
+        for (int i = 0; i < this.vector.length&&tokkens.hasMoreTokens(); i++) {
+            this.vector[i] = Integer.parseInt(tokkens.nextToken()); 
+        }
+    }
+
+    public Vector(int[] vector) {
+        super();
+        this.vector = vector;
+    }
+    
+    public Vector(int dimension) {
+        
+        super();
+        vector=new int [dimension];
+       this.setVector();
+    }
+    
+    public Vector() {
+    }
+    
+     @Override
+    public String toString() {
+        return "vector: "+Arrays.toString(vector); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
